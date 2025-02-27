89687e53c260caac770582c420449622d316b496
fanecovsf <gustavo_h07@hotmail.com>
2025-02-27 06:30:00 -0300

    feat: migration criada para person

---
+++ src/infrastructure/database/prisma/migrations/20250227092913_person/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "legalPerson" BOOLEAN NOT NULL,
+    "document" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "latitude" REAL NOT NULL,
+    "longitude" REAL NOT NULL,
+    "polygon" TEXT NOT NULL,
+    "isHeadquarter" BOOLEAN NOT NULL,
+    "isOrigin" BOOLEAN NOT NULL,
+    "isDestiny" BOOLEAN NOT NULL,
+    "isCarrier" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME,
+    "deletedAt" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "PersonTag" (
+    "personId" TEXT NOT NULL,
+    "tagId" TEXT NOT NULL,
+
+    PRIMARY KEY ("personId", "tagId"),
+    CONSTRAINT "PersonTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "PersonTag_personId_fkey" FOREIGN KEY ("personId") REFERENCES "Person" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Tag" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME,
+    "deletedAt" DATETIME,
+    "personId" TEXT,
+    CONSTRAINT "Tag_personId_fkey" FOREIGN KEY ("personId") REFERENCES "Person" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Tag" ("createdAt", "deletedAt", "id", "name", "updatedAt") SELECT "createdAt", "deletedAt", "id", "name", "updatedAt" FROM "Tag";
+DROP TABLE "Tag";
+ALTER TABLE "new_Tag" RENAME TO "Tag";
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
